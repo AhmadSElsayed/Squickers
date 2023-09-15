16049f0012b60e864ea6d87a5febfe61b0cc9413
AhmadSameh <AhmadSameh>
2023-09-15 17:46:39 +0300

    The triumph, The sacrifice

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -20,19 +20,19 @@ public class End : MonoBehaviour
         {
             if (SceneManager.GetActiveScene().name == "Level 1")
             {
-                lightVictory.text = "YOU WIN!";
-                darkVictory.text = "YOU SURVIVED!";
+                lightVictory.text = "Goal Reached!";
+                darkVictory.text = "Price Paid!";
                 StartCoroutine(WaitForSceneLoad("Level 2"));
             } else if (SceneManager.GetActiveScene().name == "Level 2")
             {
-                lightVictory.text = "YOU WIN!";
-                darkVictory.text = "YOU SURVIVED!";
+                lightVictory.text = "Level Cleared!";
+                darkVictory.text = "Soul Shattered!";
                 StartCoroutine(WaitForSceneLoad("Level 3"));
             } 
             else if (SceneManager.GetActiveScene().name == "Level 3")
             {
-                lightVictory.text = "YOU WIN!";
-                darkVictory.text = "YOU SURVIVED!";
+                lightVictory.text = "Success Gained!";
+                darkVictory.text = "Trust Broken!";
             }
         }
     }
@@ -44,8 +44,29 @@ public class End : MonoBehaviour
     
     private IEnumerator LevelStartMessage(string sceneName)
     {
-        lightVictory.text = sceneName;
-        darkVictory.text = sceneName;
+        string lights = "";
+        string darks = "";
+        
+        switch (sceneName)
+        {
+            case "Level 1":
+                lights = "The Beginning";
+                darks = "The End";
+                break;
+            
+            case "Level 2":
+                lights = "The Challenge";
+                darks = "The Despair";
+                break;
+            
+            case "Level 3":
+                lights = "The Triumph";
+                darks = "The Sacrifice";
+                break;
+        }
+        
+        lightVictory.text = lights;
+        darkVictory.text = darks;
         yield return new WaitForSeconds(2);
         lightVictory.text = "";
         darkVictory.text = "";
--- Assets/Scripts/KillerSea.cs
+++ Assets/Scripts/KillerSea.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class KillerSea : MonoBehaviour
 {
     public Text lightVictory;
     public Text darkVictory;
+    private List<string> lights = new List<string>() {"Effort Shown!", "Keep Trying!", "Potential Seen!", "Well Done!"};
+    private List<string> darks = new List<string>() {"Failure Faced!", "Mistakes Made!", "Try Harder!", "Dreams Unreached!"};
     
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player"))
         {
-            lightVictory.text = "You Lose!";
-            darkVictory.text = "You Can't Swim!";
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            lightVictory.text = lights[rand.Next(0, lights.Count)];
+            darkVictory.text = darks[rand.Next(0, darks.Count)];
+            
             Destroy(other.gameObject);
             StartCoroutine(WaitForSceneLoad(SceneManager.GetActiveScene().name));
         }
@@ -24,7 +30,7 @@ public class KillerSea : MonoBehaviour
     
     private IEnumerator WaitForSceneLoad(string sceneName)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(4);
         SceneManager.LoadScene(sceneName);
     }
 }
