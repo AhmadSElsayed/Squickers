ab1e004d556ba2076a0da189a20c053b6c6bed72
Abdelrahman Eed <abdoeed23@gmail.com>
2023-09-15 15:46:39 +0300

    Fix player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,70 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10.0f; //Controls velocity multiplier
-    public float jumpStrength = 5.0f; //Controls velocity multiplier
-    public float floatStrength = 2.0f;
-    public Camera cam;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
 
-    Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
-    private Vector3 speedV = new Vector3();
-    private bool isWalking = false;
-    private float hitDistance = 0.0f;
-    private bool onGround = false;
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        cam = Camera.main;
-        rb = GetComponent<Rigidbody>(); //rb equals the rigidbody on the player
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal") * speed; // d key changes value to 1, a key changes value to -1
-        float zMove = Input.GetAxisRaw("Vertical") * speed; // w key changes value to 1, s key changes value to -1
-        float yMove = Input.GetButtonDown("Jump") ? speed : 0f;
-
-        var speedVector = new Vector3(xMove, rb.velocity.y, zMove); // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
-        
-        if (Physics.Raycast(transform.position, Vector3.down, out var groundHit))
-        {                
-            hitDistance = groundHit.distance;
-            if (groundHit.distance > 1.5f) 
-            {
-                // You are falling or at least x units above something.
-                onGround = false;
-            } else 
-            {
-                // You are not x units above something.
-                onGround = true;
-            }
-        }
-        else
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
         {
-            onGround = false;
+            playerVelocity.y = 0f;
         }
 
-        if (onGround && Input.GetButtonDown("Jump"))
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
         {
-            speedVector.y = jumpStrength;
-        }
-        
-        // Character is trying to move while on the ground
-        if ((speedVector.x != 0f || speedVector.z != 0f) && speedVector.y == 0f) 
-        {
-            speedVector.y = (yMove == 0f) ? floatStrength : yMove;
-            isWalking = true;
-        }
-        else
-        {
-            isWalking = false;
+            gameObject.transform.forward = move;
         }
 
-        speedV = speedVector;
-        rb.velocity = speedVector;
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
