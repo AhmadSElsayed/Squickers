1e519dffaa485d724e9747d0af98d309aad32821
AhmadSameh <AhmadSameh>
2023-09-30 19:50:48 +0300

    added todos

---
+++ Assets/Dev/ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace Dev
+{
+    public class ToDo 
+    {
+        // TODO: Make a Universe prefab
+        // TODO: Make a Scene entity (maybe prefab)
+        // TODO: Fix lever animation
+        // TODO: Fix Player movement
+        // TODO: Better Camera?
+        // TODO: Better assets in general
+        // TODO: Revamp models/prefabs for easier level building
+    }
+}
